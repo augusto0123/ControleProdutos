fc92bf211c36626a08ea8c6ef4cdc93db58afee3
Augusto <senescal.oliveira@gmail.com>
2025-02-15 10:15:25 -0300

    feat: adicao do calculo de imposto

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,14 +2,17 @@ package main;
 
 import controle.ControleProdutos;
 import produtos.Eletronico;
+import produtos.Livro;
 
 public class Main {
     public static void main(String[] args){
         ControleProdutos controleProdutos = new ControleProdutos();
 
         Eletronico eletronico = new Eletronico(1, "Smartphone", 3500, 0.3);
+        Livro livro = new Livro(2, "Livro de Geografia", 50, "Editora Brasil");
 
         controleProdutos.adicionarProduto(eletronico);
+        controleProdutos.adicionarProduto(livro);
 
         controleProdutos.listarProdutos();
     }
