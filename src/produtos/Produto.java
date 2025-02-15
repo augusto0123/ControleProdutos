75d4562b73b1c6dafe172c23bce096f95b7028d9
Augusto <senescal.oliveira@gmail.com>
2025-02-15 09:17:01 -0300

    feat: adicionado escopo inicial dos produtos

---
+++ src/produtos/Produto.java
@@ -0,0 +1,13 @@
+package produtos;
+
+public abstract class Produto {
+    protected int id;
+    protected String nome;
+    protected double preco;
+
+    public Produto(int id, String nome, double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+}
